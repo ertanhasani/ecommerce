f6f98c0e8e864fbd8d0477654d8d3a4481c08528
mehrad ghane <mehrad.ghane80@gmail.com>
2022-09-26 11:46:44 +0330

    redesign header & footer

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,32 +1,32 @@
 ﻿$(document).ready(function () {
 
-    const persianToEnglishNumber = function (strNum) {
-        const pn = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-        const en = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        const an = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
-        let cache = strNum;
-        for (let i = 0; i < 10; i++) {
-            let regex_fa = new RegExp(pn[i], 'g');
-            let regex_ar = new RegExp(an[i], 'g');
-            cache = cache.replace(regex_fa, en[i]);
-            cache = cache.replace(regex_ar, en[i]);
-        }
-        return cache;
-    };
-
-    const englishToPersianNumber = function (strNum) {
-        const en = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-        const pn = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        const an = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
-        let cache = strNum;
-        for (let i = 0; i < 10; i++) {
-            let regex_fa = new RegExp(pn[i], 'g');
-            let regex_ar = new RegExp(an[i], 'g');
-            cache = cache.replace(regex_fa, en[i]);
-            cache = cache.replace(regex_ar, en[i]);
-        }
-        return cache;
-    };
+    // const persianToEnglishNumber = function (strNum) {
+    //     const pn = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+    //     const en = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    //     const an = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
+    //     let cache = strNum;
+    //     for (let i = 0; i < 10; i++) {
+    //         let regex_fa = new RegExp(pn[i], 'g');
+    //         let regex_ar = new RegExp(an[i], 'g');
+    //         cache = cache.replace(regex_fa, en[i]);
+    //         cache = cache.replace(regex_ar, en[i]);
+    //     }
+    //     return cache;
+    // };
+    //
+    // const englishToPersianNumber = function (strNum) {
+    //     const en = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+    //     const pn = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    //     const an = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
+    //     let cache = strNum;
+    //     for (let i = 0; i < 10; i++) {
+    //         let regex_fa = new RegExp(pn[i], 'g');
+    //         let regex_ar = new RegExp(an[i], 'g');
+    //         cache = cache.replace(regex_fa, en[i]);
+    //         cache = cache.replace(regex_ar, en[i]);
+    //     }
+    //     return cache;
+    // };
 
     const priceToPersian = function () {
 
